f3d81d89c1012ca5a7e093223af3d1ffab9e3fcc
Rizki CL <captain.neon99@gmail.com>
2019-07-09 19:45:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/flexoralphastage/FirstOpenCarouselActivity.java
@@ -0,0 +1,14 @@
+package com.example.flexoralphastage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstOpenCarouselActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_open_carousel);
+    }
+}
